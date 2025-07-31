993dd7eff31218a49601d580ca8edcbcc609965f
Arthur <93857989+Arthurprnt@users.noreply.github.com>
2025-07-31 20:00:57 +0200

    Started to add code

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@onready var spawnPos: Node2D = $LevelBackgound/SpawnPos
+@onready var levelBackgound: Node2D = $LevelBackgound
 @onready var playButton: Button = $VBoxContainer/MarginContainer/VBoxContainer/PlayButton
 @onready var blackFade: ColorRect = $BlackFade
 
+@onready var playerScene: PackedScene = preload("res://scenes/objects/player.tscn")
+
+var code: Array[String] = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a"]
+var currentInputs: Array[String] = []
+
 var exit: bool = false
 var exitValue: float = 1.8
+
 
 func _ready() -> void:
 	playButton.grab_focus()
@@ -14,6 +22,20 @@ func _on_play_button_pressed() -> void:
 
 func _on_button_pressed() -> void:
 	exit = true
+
+func _process(delta: float) -> void:
+	print(currentInputs)
+	for action in InputMap.get_actions():
+		if str(action) in ["left", "right", "up", "down", "a", "b"]:
+			if Input.is_action_just_pressed(action):
+				currentInputs.append(str(action))
+		elif Input.is_action_just_pressed(action) && !(str(action) in ["ui_left", "ui_right", "ui_up", "ui_down", "ui_text_caret_left", "ui_text_caret_right", "ui_text_caret_up", "ui_text_caret_down"]):
+				currentInputs = []
+	if currentInputs == code:
+		var player: CharacterBody2D = playerScene.instantiate()
+		player.global_position = 4 * spawnPos.global_position
+		player.get_child(0).queue_free()
+		levelBackgound.add_child(player)
 
 func _physics_process(delta: float) -> void:
 	if exit:
